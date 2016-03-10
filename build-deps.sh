9dc21b55a8d24565d745abf634cbbbd797d673fd
Seth Parker <csparker247@gmail.com>
2016-03-10 15:32:54 -0500

    Add Linux options so we can compile on Debian

--- build-deps.sh
+++ build-deps.sh
@@ -52,6 +52,8 @@ BUILD_DIR="${ENV_ROOT}/build"
 TARGET_DIR="${ENV_ROOT}/deps"
 CMAKE_PREFIX=""
 CONFIGURE_PREFIX=""
+BOOST_BOOTSTRAP_CLANG=""
+BOOST_B2_CLANG=""
 OSX_SDK_VERSION=""
 OSX_CMAKE_SDK=""
 OSX_BOOST_SDK=""
@@ -89,7 +91,7 @@ if [[ "$platform" == "macosx" ]] && [[ $universal == true ]]; then
     OSX_BOOST_SDK="macosx-version=${OSX_SDK_VERSION} macosx-version-min=${OSX_SDK_VERSION}"
 fi
 
-${ENV_ROOT}/fetchurl "https://downloads.sourceforge.net/project/boost/boost/1.58.0/boost_1_58_0.tar.bz2"
+${ENV_ROOT}/fetchurl "https://downloads.sourceforge.net/project/boost/boost/1.58.0/boost_1_58_0.tar.gz"
 ${ENV_ROOT}/fetchurl "http://www.vtk.org/files/release/6.3/VTK-6.3.0.tar.gz"
 ${ENV_ROOT}/fetchurl "https://github.com/valette/ACVD/archive/vtk6.tar.gz"
 ${ENV_ROOT}/fetchurl "https://github.com/Itseez/opencv/archive/2.4.12.tar.gz"
@@ -97,7 +99,11 @@ ${ENV_ROOT}/fetchurl "https://downloads.sourceforge.net/project/itk/itk/4.8/Insi
 ${ENV_ROOT}/fetchurl "https://github.com/bulletphysics/bullet3/archive/2.83.6.tar.gz"
 ${ENV_ROOT}/fetchurl "http://bitbucket.org/eigen/eigen/get/3.2.6.tar.bz2"
 ${ENV_ROOT}/fetchurl "https://github.com/mariusmuja/flann/archive/1.8.0-src.tar.gz"
-${ENV_ROOT}/fetchurl "https://github.com/PointCloudLibrary/pcl/archive/pcl-1.7.2.tar.gz"
+if [[ "$platform" == "linux" ]]; then
+    ${ENV_ROOT}/fetchurl "https://github.com/PointCloudLibrary/pcl/archive/pcl-1.8.0rc2.tar.gz"
+else
+    ${ENV_ROOT}/fetchurl "https://github.com/PointCloudLibrary/pcl/archive/pcl-1.7.2.tar.gz"
+fi
 
 echo "*** Building boost ***"
 cd $BUILD_DIR/boost*
@@ -112,10 +118,13 @@ using darwin : ${OSX_SDK_VERSION}
 : <target-os>darwin
 ;
 EOF
+elif [[ "$platform" == "linux" ]]; then
+    BOOST_BOOTSTRAP_CLANG="--with-toolset=clang"
+    BOOST_B2_CLANG="toolset=clang"
 fi
 BOOST_LIBS="atomic,chrono,date_time,exception,iostreams,filesystem,program_options,random,serialization,signals,system,test,thread"
-./bootstrap.sh --prefix=${TARGET_DIR} --with-libraries=$BOOST_LIBS && \
-./b2 -j ${jval} cxxflags="-arch x86_64" variant=release link=static ${OSX_BOOST_SDK} install
+./bootstrap.sh --prefix=${TARGET_DIR} --with-libraries=$BOOST_LIBS ${BOOST_BOOTSTRAP_CLANG} && \
+./b2 -j ${jval} cxxflags="-arch x86_64 -std=c++11" variant=release link=static ${BOOST_B2_CLANG} ${OSX_BOOST_SDK} install
 
 echo "*** Building VTK ***"
 cd $BUILD_DIR/VTK*
@@ -170,5 +179,5 @@ echo "*** Building PCL ***"
 cd $BUILD_DIR/pcl*
 mkdir -p build && \
 cd build/ && \
-cmake -DWITH_VTK=OFF -DWITH_QT=OFF -DBUILD_visualization=OFF -DBUILD_tools=OFF -DPCL_SHARED_LIBS=OFF -DCMAKE_BUILD_TYPE=Release ${CMAKE_PREFIX} ${OSX_CMAKE_SDK} .. && \
+cmake -DWITH_VTK=OFF -DWITH_QT=OFF -DBUILD_visualization=OFF -DBUILD_tools=OFF -DPCL_SHARED_LIBS=OFF -DWITH_OPENGL=OFF -DCMAKE_BUILD_TYPE=Release ${CMAKE_PREFIX} ${OSX_CMAKE_SDK} .. && \
 make -j${jval} install
