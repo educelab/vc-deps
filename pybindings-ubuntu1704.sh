089f959937ef2392e2be6e9de5d25926ddd1992f
Seth Parker <csparker247@gmail.com>
2017-12-05 11:30:23 -0500

    Fix some small build bugs in the ubuntu script

--- pybindings-ubuntu1704.sh
+++ pybindings-ubuntu1704.sh
@@ -4,7 +4,7 @@
 apt-get update
 apt-get install -y ninja-build cmake python3 python3-pip qtbase5-dev \
                 build-essential libboost-all-dev libeigen3-dev libflann-dev \
-                git curl cmake-curses-gui
+                git curl cmake-curses-gui libxt-dev
 pip3 install numpy
 
 # Setup our working directory
@@ -16,48 +16,50 @@ rm -rf *
 cd ~/source/
 curl -L -O "https://downloads.sourceforge.net/project/itk/itk/4.12/InsightToolkit-4.12.2.tar.gz"
 tar -xzf InsightToolkit-4.12.2.tar.gz
-cd InsightToolkit-4.12.2
+cd InsightToolkit*/
 mkdir -p build
 cd build/
-cmake -GNinja ..
-ninja && ninja install
+cmake -GNinja .. && \
+ninja install
 
 # VTK
 cd ~/source/
 curl -L -O "http://www.vtk.org/files/release/8.0/VTK-8.0.1.tar.gz"
 tar -xzf VTK-8.0.1.tar.gz
-cd VTK-8.0.1
+cd VTK*/
 mkdir -p build
 cd build/
-cmake -GNinja ..
-ninja && ninja install
+cmake -GNinja .. && \
+ninja install
 
 # ACVD
 cd ~/source/
 curl -L -O "https://github.com/valette/ACVD/archive/d5d8c16.tar.gz"
 tar -xzf d5d8c16.tar.gz
-cd ACVD/
+cd ACVD*/
 mkdir -p build
 cd build/
-cmake -GNinja ..
-ninja && ninja install
+cmake -GNinja .. && \
+ninja install
 
 # OpenCV
 cd ~/source/
 curl -L -O "https://github.com/opencv/opencv/archive/3.3.1.tar.gz"
 tar -xzf 3.3.1.tar.gz
-cd opencv-3.3.1/
+cd opencv*/
 mkdir -p build
 cd build/
-cmake -GNinja ..
-ninja && ninja install
+cmake -GNinja .. && \
+ninja install
 
 # pybind11
 cd ~/source/
 curl -L -O "https://github.com/pybind/pybind11/archive/a303c6f.tar.gz"
 tar -xzf a303c6f.tar.gz
-cd pybind11/
+cd pybind11*/
 mkdir -p build
 cd build/
-cmake -GNinja ..
-ninja && ninja install
+cmake -DPYBIND11_TEST=OFF -GNinja .. && \
+ninja install
+
+cd ~/source/
