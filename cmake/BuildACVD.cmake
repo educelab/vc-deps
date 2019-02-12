54bd86eb2fa38e2de37cbc3cb23df4c7c707fdff
Seth Parker <csparker247@gmail.com>
2019-02-12 11:20:08 -0500

    Update ACVD (fixes #12), update exiv2 (fixes #13), update Boost -> 1.69.

--- cmake/BuildACVD.cmake
+++ cmake/BuildACVD.cmake
@@ -3,13 +3,12 @@ if(VCDEPS_BUILD_ACVD)
 externalproject_add(
     acvd
     DEPENDS vtk ${GLOBAL_DEPENDS}
-    URL https://github.com/valette/ACVD/archive/d5d8c16.tar.gz
-    URL_HASH SHA1=c9655ff995058f31af37479cde08ae5dfedeecee
+    URL https://github.com/csparker247/ACVD/archive/v1.0.1.tar.gz
+    URL_HASH SHA256=eb6ef33a56f8086f208bf39ebc85f5f970c24d48fb6a304e94bc08f6c2c1eaf3
     DOWNLOAD_NO_PROGRESS true
     CMAKE_CACHE_ARGS
         ${GLOBAL_CMAKE_ARGS}
         -DBUILD_EXAMPLES:BOOL=OFF
-        -DCMAKE_BUILD_TYPE:STRING=Release
 )
 else()
   add_custom_target(acvd)
--- cmake/BuildExiv2.cmake
+++ cmake/BuildExiv2.cmake
@@ -4,10 +4,9 @@ find_package(Intl QUIET)
 externalproject_add(
     exiv2
     DEPENDS ${GLOBAL_DEPENDS}
-    URL http://www.exiv2.org/builds/exiv2-0.27.0a-Source.tar.gz
-    URL_HASH SHA256=a4adfa7aaf295b0383adead476f8e0493b9d6c6c7570d5884d2ebf8a2871902f
+    URL https://github.com/Exiv2/exiv2/archive/75e1a03.tar.gz
+    URL_HASH SHA256=d6661d4057d67228345a201fa9a9ba70a64bee62d9922472697dfca53a0f2945
     DOWNLOAD_NO_PROGRESS true
-    PATCH_COMMAND patch -p1 -i ${CMAKE_SOURCE_DIR}/patches/exiv2.0.27.0-cmake-libintl.diff
     CMAKE_CACHE_ARGS
         ${GLOBAL_CMAKE_ARGS}
         -DEXIV2_BUILD_SAMPLES:BOOL=OFF
