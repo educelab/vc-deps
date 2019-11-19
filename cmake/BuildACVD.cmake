db81740c3a28452712e90f2a2821b7b4f69a87d0
Seth Parker <csparker247@gmail.com>
2019-11-19 15:42:51 -0500

    Update ACVD 1.1.1

--- cmake/BuildACVD.cmake
+++ cmake/BuildACVD.cmake
@@ -3,8 +3,8 @@ if(VCDEPS_BUILD_ACVD)
 externalproject_add(
     acvd
     DEPENDS vtk ${GLOBAL_DEPENDS}
-    URL https://github.com/csparker247/ACVD/archive/v1.1.0.tar.gz
-    URL_HASH SHA256=6c9b36f7b595e2ec3763b9f11ac1902f05477ba12a76f0230bd3c886aac7585e
+    URL https://github.com/csparker247/ACVD/archive/v1.1.1.tar.gz
+    URL_HASH SHA256=077dd3fc7a6bd581377d670331a6fb5aefe7bb1cd8b3e65bd2058195dc0069d1
     DOWNLOAD_NO_PROGRESS true
     CMAKE_CACHE_ARGS
         ${GLOBAL_CMAKE_ARGS}
