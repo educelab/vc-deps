4e8e213e5815712b1afca5ee47fbe40c9085a24e
Seth Parker <csparker247@gmail.com>
2023-12-14 12:28:57 -0500

    Revert libtiff to vc-deps 1.7 version

--- cmake/BuildTIFF.cmake
+++ cmake/BuildTIFF.cmake
@@ -3,8 +3,8 @@ if(VCDEPS_BUILD_TIFF)
 externalproject_add(
     libtiff
     DEPENDS zlib ${GLOBAL_DEPENDS}
-    URL https://gitlab.com/libtiff/libtiff/-/archive/3705f82b6483c7906cf08cd6b9dcdcd59c61d779/libtiff-3705f82b6483c7906cf08cd6b9dcdcd59c61d779.tar.gz
-    URL_HASH SHA512=3f28ea3763380e730a3b3220d173b4519cf8c9d69e6312e1364599233e11cfa4707ebc98484a07e63ed24a7ab3ada53da397e160da0f8ebc78cb6045e61d693d
+    URL https://gitlab.com/libtiff/libtiff/-/archive/5e18004500cda10d9074bdb6166b054e95b659ed/libtiff-5e18004500cda10d9074bdb6166b054e95b659ed.tar.gz
+    URL_HASH SHA512=cf19fbfcc278fe9d113035db0aa4df56f461ba311394c40d43dd5da9f9a3e4a399acf885c57c3511e16cc396a90db0965e99440cebc40f45ac1e404e59196d08
     DOWNLOAD_NO_PROGRESS true
     DOWNLOAD_EXTRACT_TIMESTAMP ON
     CMAKE_CACHE_ARGS
