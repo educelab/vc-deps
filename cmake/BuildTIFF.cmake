4645fea9a692db5fa568e42022e58efbe05bfa05
Seth Parker <csparker247@gmail.com>
2023-12-14 11:59:16 -0500

    (TIFF) Head commit which fixes cmake prefix issues

--- cmake/BuildTIFF.cmake
+++ cmake/BuildTIFF.cmake
@@ -3,8 +3,8 @@ if(VCDEPS_BUILD_TIFF)
 externalproject_add(
     libtiff
     DEPENDS zlib ${GLOBAL_DEPENDS}
-    URL https://gitlab.com/libtiff/libtiff/-/archive/v4.6.0/libtiff-v4.6.0.tar.gz
-    URL_HASH SHA512=ef2f1d424219d9e245069b7d23e78f5e817cf6ee516d46694915ab6c8909522166f84997513d20a702f4e52c3f18467813935b328fafa34bea5156dee00f66fa
+    URL https://gitlab.com/libtiff/libtiff/-/archive/3705f82b6483c7906cf08cd6b9dcdcd59c61d779/libtiff-3705f82b6483c7906cf08cd6b9dcdcd59c61d779.tar.gz
+    URL_HASH SHA512=3f28ea3763380e730a3b3220d173b4519cf8c9d69e6312e1364599233e11cfa4707ebc98484a07e63ed24a7ab3ada53da397e160da0f8ebc78cb6045e61d693d
     DOWNLOAD_NO_PROGRESS true
     DOWNLOAD_EXTRACT_TIMESTAMP ON
     CMAKE_CACHE_ARGS
