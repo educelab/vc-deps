ce5024b6e6861bcd04662b72c46e00977a851bc1
Seth Parker <csparker247@gmail.com>
2024-08-05 09:39:43 -0400

    Revert Eigen update

--- cmake/BuildEigen.cmake
+++ cmake/BuildEigen.cmake
@@ -3,8 +3,8 @@ if(VCDEPS_BUILD_EIGEN)
 externalproject_add(
     eigen
     DEPENDS boost ${GLOBAL_DEPENDS}
-    URL https://gitlab.com/libeigen/eigen/-/archive/c593e9e948e5c10234afbac7f6d143ecd9f0c680/eigen-c593e9e948e5c10234afbac7f6d143ecd9f0c680.tar.gz
-    URL_HASH SHA512=e1033869457844861f7bb134f503e2cec4c945843c840b913f7be9ec73cb8d261592d953be9b5199a2d6528ad4eed2dbf3b353bc91cebb5159524192edd7f80c
+    URL https://gitlab.com/libeigen/eigen/-/archive/3.4.0/eigen-3.4.0.tar.gz
+    URL_HASH SHA512=ba75ecb760e32acf4ceaf27115468e65d4f77c44f8d519b5a13e7940af2c03a304ad433368cb6d55431f307c5c39e2666ab41d34442db3cf441638e51f5c3b6a
     DOWNLOAD_NO_PROGRESS true
     DOWNLOAD_EXTRACT_TIMESTAMP ON
     CMAKE_CACHE_ARGS
