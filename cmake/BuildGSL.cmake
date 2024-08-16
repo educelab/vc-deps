75b3492288e3983b8c2f313c1cdf6e3e9762e68f
Seth Parker <csparker247@gmail.com>
2024-08-16 13:18:23 -0400

    Try quoting

--- cmake/BuildGSL.cmake
+++ cmake/BuildGSL.cmake
@@ -10,7 +10,7 @@ endif()
 
 
 if(BUILD_MACOS_MULTIARCH)
-    set(GSL_CFLAGS "CFLAGS='-arch x86_64 -arch arm64'")
+    set(GSL_CFLAGS "CFLAGS=\"-arch x86_64 -arch arm64\"")
 endif()
 
 externalproject_add(
@@ -21,7 +21,7 @@ externalproject_add(
     DOWNLOAD_NO_PROGRESS true
     DOWNLOAD_EXTRACT_TIMESTAMP ON
     PATCH_COMMAND ${GSL_PATCH_CMD}
-    CONFIGURE_COMMAND ${GSL_CFLAGS} ./configure --prefix=${GSL_INSTALL_PREFIX} ${GSL_WITH_PIC}
+    CONFIGURE_COMMAND "${GSL_CFLAGS} ./configure --prefix=${GSL_INSTALL_PREFIX} ${GSL_WITH_PIC}"
     BUILD_COMMAND make install
     BUILD_IN_SOURCE true
     INSTALL_COMMAND ""
